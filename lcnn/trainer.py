e193a001d2fea45b76df4d11d4fb531343408d1b
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-07 15:23:20 +0200

    fixed draw_vecl

--- lcnn/trainer.py
+++ lcnn/trainer.py
@@ -311,7 +311,7 @@ class Trainer(object):
         lpre_label = meta[i]["lpre_label"].cpu().numpy()
         #print("lpre_label", lpre_label.shape)
         lpre_label = meta[i]["lpre_label"].cpu().numpy()
-        print("vecl target max", np.max(lpre_label), lpre_label)
+        #print("vecl target max", np.max(lpre_label), lpre_label)
         vecl_result = result["lines"][i].cpu().numpy() * 4
         #print("results for lines",vecl_result.shape, vecl_result[1])
         score = result["score"][i].cpu().numpy()
