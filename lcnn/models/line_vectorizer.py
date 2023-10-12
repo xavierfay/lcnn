39d5b2aca71c94da120842fc5a08b1ab7877240a
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-12 16:42:42 +0200

    filter before construct line

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -279,7 +279,7 @@ class LineVectorizer(nn.Module):
 
             #sample lines
             u, v, label = u[c], v[c], label[c]
-            xy = [xy[i, score[i] > 0.1] for i in range(n_type)]
+            xy = torch.cat([xy[i, score[i] > 0.1][None, :] for i in range(n_type)], dim=0)
             xy = xy.reshape(n_type * K, 2)
             xyu, xyv = xy[u], xy[v]
 
