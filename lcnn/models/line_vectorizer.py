5fe8a318e7d26b38293b957e91b2b11225afdaeb
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-17 21:34:30 +0200

    filter out part 2

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -254,80 +254,47 @@ class LineVectorizer(nn.Module):
             # u, v = u.flatten(), v.flatten()
             # up, vp = match[u], match[v]
 
-            threshold = 0.03  # Define your threshold value here
+            threshold = 0.03
 
+            # Get top K scores and their indices
             score, index = torch.topk(jmap, k=K)
 
             # Create a mask for scores that are higher than the threshold
             mask = score > threshold
 
-            # Filter out indices, scores, joff, and junc based on the mask
+            # Filter out indices and scores based on the mask
             filtered_score = score[mask]
             filtered_index = index[mask]
-            # Expand the filtered_index to match the batch size and add an extra dimension at the end
-            # Adjust the shape of filtered_index
-            print(junc.shape)
-            print(filtered_index.shape)
-            expanded_index = filtered_index.unsqueeze(0).unsqueeze(1).expand(2, 2, -1)
-            # Gather values from joff along the third dimension
-            filtered_joff = torch.gather(joff, 2, expanded_index)
 
-            # Adjust the shape of filtered_index for junc
-            expanded_index_for_junc = filtered_index.unsqueeze(-1)
-
-            # Gather values from junc along the first dimension
-            filtered_junc = torch.gather(junc, 0, expanded_index_for_junc)
+            # Gather values from joff and junc
+            filtered_joff = torch.gather(joff, 2, filtered_index.unsqueeze(0).unsqueeze(1).expand(2, 2, -1))
+            filtered_junc = torch.gather(junc, 0, filtered_index.unsqueeze(-1))
 
             # Calculate x, y based on the filtered indices
-            # Adjust the shape of filtered_index for the batch size
-            # Adjust the shape of filtered_index to match the batch size
-            expanded_index_batched = filtered_index.unsqueeze(0).expand(2, -1)
+            y_values = torch.gather(filtered_joff[:, 0], 1, filtered_index.unsqueeze(0).expand(2, -1))
+            x_values = torch.gather(filtered_joff[:, 1], 1, filtered_index.unsqueeze(0).expand(2, -1))
 
-            # Gather values from filtered_joff[:, 0] using the batched expanded_index
-            y_values = torch.gather(filtered_joff[:, 0], 1, expanded_index_batched)
-
-            print(filtered_joff[:, 0].unsqueeze(-1).shape)
-            print(expanded_index.shape)
-            print(expanded_index.min())
-            print(expanded_index.max())
-
-
-
-            # Continue with your calculation
-            y = (filtered_index // 256).float() + y_values.squeeze() + 0.5
-            x_values = torch.gather(filtered_joff[:, 1], 1, expanded_index_batched)
-
-            # Continue with your calculation for x
-            x = (filtered_index % 256).float() + x_values.squeeze() + 0.5
+            y = (filtered_index // 128).float() + y_values.squeeze() + 0.5
+            x = (filtered_index % 128).float() + x_values.squeeze() + 0.5
 
             # xy: [N_TYPE, K, 2]
             xy = torch.cat([y[..., None], x[..., None]], dim=-1)
             xy_ = xy[..., None, :]
-            del x, y, filtered_index
 
             # dist: [N_TYPE, K, N]
             dist = torch.sum((xy_ - filtered_junc) ** 2, -1)
             cost, match = torch.min(dist, -1)
 
-            # xy: [N_TYPE * K, 2]
-            # match: [N_TYPE, K]
-            for t in range(n_type):
-                match[t, jtyp[match[t]] != t] = N
+            # Filtering matches based on conditions
+            match[(jtyp[match] != torch.arange(n_type).unsqueeze(1)).any(dim=0)] = N
             match[cost > 1.5 * 1.5] = N
             match = match.flatten()
 
-            _ = torch.arange(n_type * K, device=device)
-            u, v = torch.meshgrid(_, _)
-            u, v = u.flatten(), v.flatten()
-            up, vp = match[u], match[v]
-
-            scalar_labels = Lpos[up, vp]
-            scalar_labels = scalar_labels.long()
-            # Initialize a tensor of zeros with shape [N, 3]
-            label = torch.zeros(scalar_labels.shape[0], 3, device=scalar_labels.device)
-
-            # Assign a "1" in the respective column according to the scalar label
-            label[torch.arange(label.shape[0]), scalar_labels] = 1
+            # Compute scalar_labels and the label tensor
+            u, v = torch.meshgrid(torch.arange(n_type * K, device=device), repeat=2)
+            up, vp = match[u.flatten()], match[v.flatten()]
+            scalar_labels = Lpos[up, vp].long()
+            label = torch.zeros_like(scalar_labels, dtype=torch.float).scatter_(1, scalar_labels.unsqueeze(-1), 1)
 
             if mode == "training":
                 c = torch.zeros_like(label[:, 0], dtype=torch.bool)
