4250f46b6b9bc400e4a68e88932008811788483a
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-19 17:11:03 +0200

    lmap loss fix

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -47,6 +47,7 @@ class LineVectorizer(nn.Module):
         h = result["preds"]
         losses = result["losses"]
         lmap_losses = [l['lmap'] for l in losses]
+        lmap_losses = torch.stack(lmap_losses, dim=1)
         print(lmap_losses)
         x = self.fc1(result["feature"])
         n_batch, n_channel, row, col = x.shape
