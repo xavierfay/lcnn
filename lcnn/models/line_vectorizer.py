9f2136eb5ac9cb1d2c94e35e4e5195c2393ac6ab
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-21 15:22:26 +0200

    print Lpos

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -291,7 +291,7 @@ class LineVectorizer(nn.Module):
 
                 print(Lpos)
                 unique_Lpos = torch.unique(Lpos)
-                count_Lpos = torch.bincount(Lpos)
+                count_Lpos = torch.bincount(Lpos.flatten())
                 for value, freq in zip(unique_Lpos, count_Lpos):
                     print(f"Value: {value}, Count: {freq}")
 
