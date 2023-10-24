ea70d24c93db911bc0d58cae27f213dd8b8df36b
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-24 22:00:01 +0200

    larger sample size for cpu run

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -310,9 +310,7 @@ class LineVectorizer(nn.Module):
             #jcs = [xy[i, score[i].long()] for i in range(n_type)]
             jcs = [xy[i, score[i] > 0.001] for i in range(n_type)]
 
-            if mode != "training":
 
-                print(line.shape)
             return line, label, jcs
 
 
