5aa6ebea30ab756500106581e902dfbc368e278c
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-25 14:46:32 +0200

    remove half and add nan

--- train.py
+++ train.py
@@ -135,9 +135,6 @@ def main():
         model.load_state_dict(checkpoint["model_state_dict"])
     model = model.to(device)
 
-    if M.use_half and device == torch.device("cuda"):
-        model = model.half()
-
     # 3. optimizer
     if C.optim.name == "Adam":
         optim = torch.optim.Adam(
