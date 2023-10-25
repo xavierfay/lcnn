dda8627040aa5d724e2d8892f7ed6289e340cf4e
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-25 14:28:26 +0200

    change half

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -48,7 +48,6 @@ class LineVectorizer(nn.Module):
         x = self.fc1(result["feature"])
         n_batch, n_channel, row, col = x.shape
 
-        print(len(input_dict["meta"]), input_dict["meta"])
 
         xs, ys, fs, ps, idx, jcs = [], [], [], [], [0], []
         for i, meta in enumerate(input_dict["meta"]):
@@ -92,7 +91,6 @@ class LineVectorizer(nn.Module):
             xp = self.pooling(xp)
             xs.append(xp)
             idx.append(idx[-1] + xp.shape[0])
-        print(xs)
         x= torch.cat(xs)
 
         y = torch.cat(ys)
