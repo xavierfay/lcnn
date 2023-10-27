dadde7acca9392aef7586bd76d251a7a6137200f
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-27 15:09:11 +0200

    dyn k

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -219,6 +219,7 @@ class LineVectorizer(nn.Module):
 
             n_type = jmap.shape[0]
             N = len(junc)
+            device = jmap.device
             jmap = non_maximum_suppression(jmap).reshape(n_type, -1)
             joff = joff.reshape(n_type, 2, -1)
             #max_K = M.n_dyn_junc // n_type
