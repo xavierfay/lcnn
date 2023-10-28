fcd1592046c2d44e25b16c774723f772c8a0fc85
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-29 00:38:39 +0200

    old sampler

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -260,9 +260,10 @@ class LineVectorizer(nn.Module):
             cost, match = torch.min(dist, -1)
 
 
-            for t in range(n_type):
-                match[t, jtyp[match[t]] != t] = N
-            match[cost > 1.5 * 1.5] = N
+            # for t in range(n_type):
+            #     match[t, jtyp[match[t]] != t] = N
+            # match[cost > 1.5 * 1.5] = N
+            match[cost > 0.5 ] = N
             match = match.flatten()
 
             if mode == "testing":
@@ -321,20 +322,6 @@ class LineVectorizer(nn.Module):
             xy = xy.reshape(n_type * K, 2)
             xyu, xyv = xy[u], xy[v]
 
-            # # Compute slopes and create masks for valid lines (horizontal/vertical)
-            # deltas = xyv - xyu
-            # slopes = torch.where(deltas[:, 0] != 0, deltas[:, 1] / deltas[:, 0], float('inf'))
-            # horizontal_mask = torch.abs(slopes) < 0.001
-            # vertical_mask = torch.abs(slopes) > 10000
-            # valid_lines_mask = horizontal_mask | vertical_mask
-            # #print("shapes", valid_lines_mask.shape[0], xyu.shape[0])
-            #
-            # # Ensure that valid_lines_mask does not contain invalid indices
-            # assert valid_lines_mask.shape[0] == xyu.shape[0], "Shape mismatch between mask and data"
-            #
-            # # Filter xyu, xyv, and label using the valid_lines_mask
-            # xyu, xyv = xyu[valid_lines_mask], xyv[valid_lines_mask]
-            # label = label[valid_lines_mask]
 
             label = torch.zeros(scalar_labels.shape[0], 4, device=scalar_labels.device)
 
