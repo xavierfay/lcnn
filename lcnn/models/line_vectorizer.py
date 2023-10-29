0e0e489938192b05751bbeb8fa3f76527487dbea
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-29 11:14:31 +0100

    new match

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -309,16 +309,14 @@ class LineVectorizer(nn.Module):
             cost, match = torch.min(dist, -1)
 
             # For the first two layers, match separately
-            for t in range(2):
-                match[t, jtyp[match[t]] != t] = N
-
-            # For subsequent layers, match them together
-            combined_match = match[2:].clone()
-            for t in range(2, n_type):
-                mask = jtyp[match[t]] != t  # Generate a mask for the current layer 't'
-                combined_match[t - 2, mask] = N  # Apply the mask to the corresponding slice of 'combined_match'
-
-            match[2:] = combined_match
+            for t in range(n_type):
+                # For the first two layers, only match with the same layer
+                if t < 2:
+                    mask = jtyp[match[t]] != t
+                # For the remaining layers, match with any layer from 2 to n_type
+                else:
+                    mask = jtyp[match[t]] < 2
+                match[t, mask] = N
 
             match[cost > 1.5 * 1.5] = N
             match = match.flatten()
