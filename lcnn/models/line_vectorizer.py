dba4928ac84d1129acece7b30cb8012f17c9b0d4
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-31 14:35:12 +0100

    more prints

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -66,7 +66,7 @@ class LineVectorizer(nn.Module):
             jcs.append(jc)
             ps.append(p)
             jtypes.append(jtype)
-            print("jtypes.shape", jtypes.shape, jtypes.max(), jtypes.min())
+            print("jtypes.shape", len(jtypes), jtypes.max(), jtypes.min())
             #fs.append(feat)
 
 
