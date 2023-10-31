46d028f09ce9c7d38572a1b36cb115bd192cab2e
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-31 22:27:02 +0100

    fix plot

--- lcnn/trainer.py
+++ lcnn/trainer.py
@@ -328,7 +328,7 @@ class Trainer(object):
 
         mask_result = result["jmap"][i].cpu().detach().numpy()
 
-        #mask_result = np.sum(mask_result, axis=0)
+        mask_result = np.sum(mask_result, axis=0)
         #mask_result = plt_heatmaps(mask_result)
         mask_target = target["jmap"][i].cpu().numpy()
         mask_target = plt_heatmaps(mask_target[1:])
