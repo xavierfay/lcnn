9ce4daa3cf63804ad2beb1c609e050430f2cc7da
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-10-31 16:16:58 +0100

    other nms

--- lcnn/models/line_vectorizer.py
+++ lcnn/models/line_vectorizer.py
@@ -362,7 +362,7 @@ def nms_3d(a):
 
     # For multiple layers, apply 3D NMS
     a = a.view(1, original_shape[0], original_shape[1], original_shape[2])
-    ap = F.max_pool3d(a, (original_shape[0], 5, 5), stride=(1, 1, 1), padding=(0, 1, 1))
+    ap = F.max_pool3d(a, (original_shape[0], 5, 5), stride=(1, 1, 1), padding=(0, 2, 2))
     keep = (a == ap).float()
     return (a * keep).squeeze(0)  # Ensure it's [number_of_layers, 256, 256]
 
