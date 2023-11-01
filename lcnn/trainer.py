1c3ed4bd84a69ae8bca73d80ab69a62bb99f2fd3
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-11-01 20:35:22 +0100

    no mask

--- lcnn/trainer.py
+++ lcnn/trainer.py
@@ -296,26 +296,26 @@ class Trainer(object):
         img = io.imread(fn)
         imshow(img), plt.savefig(f"{prefix}_img.jpg"), plt.close()
 
-        jmap = (result["jmap"][i].cpu().detach().numpy())
-        for j, results in enumerate(jmap):
-            mask_result = results
-            imshow(mask_result, cmap="hot"), plt.savefig(f"{prefix}_mask_{j}b.jpg"), plt.close()
-        #mask_result = plt_heatmaps(mask_result)
-
-
-        mask_target = target["jmap"][i].cpu().numpy()
-        #mask_target = plt_heatmaps(mask_target)
-
-
-        # Displaying the results using the updated imshow function
-        #imshow(mask_target, cmap="jet"), plt.savefig(f"{prefix}_mask_a.jpg"), plt.close()
-
-        # imshow(mask_target), plt.savefig(f"{prefix}_mask_a.jpg"), plt.close()
-        # imshow(mask_result), plt.savefig(f"{prefix}_mask_b.jpg"), plt.close()
-
-        for j, mask_target in enumerate(target["jmap"][i]):
-            mask_target = mask_target.cpu().detach().numpy()
-            imshow(mask_target, cmap="hot"), plt.savefig(f"{prefix}_mask_{j}a.jpg"), plt.close()
+        # jmap = (result["jmap"][i].cpu().detach().numpy())
+        # for j, results in enumerate(jmap):
+        #     mask_result = results
+        #     imshow(mask_result, cmap="hot"), plt.savefig(f"{prefix}_mask_{j}b.jpg"), plt.close()
+        # #mask_result = plt_heatmaps(mask_result)
+        #
+        #
+        # mask_target = target["jmap"][i].cpu().numpy()
+        # #mask_target = plt_heatmaps(mask_target)
+        #
+        #
+        # # Displaying the results using the updated imshow function
+        # #imshow(mask_target, cmap="jet"), plt.savefig(f"{prefix}_mask_a.jpg"), plt.close()
+        #
+        # # imshow(mask_target), plt.savefig(f"{prefix}_mask_a.jpg"), plt.close()
+        # # imshow(mask_result), plt.savefig(f"{prefix}_mask_b.jpg"), plt.close()
+        #
+        # for j, mask_target in enumerate(target["jmap"][i]):
+        #     mask_target = mask_target.cpu().detach().numpy()
+        #     imshow(mask_target, cmap="hot"), plt.savefig(f"{prefix}_mask_{j}a.jpg"), plt.close()
 
         for j, results in enumerate(result["lmap"][i]):
             line_result = results.cpu().numpy()
