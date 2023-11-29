b91292e78662b4073b1b72dbf391549fb4af6db9
xavierfay <97184213+xavierfay@users.noreply.github.com>
2023-11-29 12:00:51 +0100

    1792 size

--- lcnn/trainer.py
+++ lcnn/trainer.py
@@ -367,18 +367,18 @@ class Trainer(object):
 
             plt.savefig(fn), plt.close()
 
-        juncs = meta[i]["junc"].cpu().numpy() * 6
+        juncs = meta[i]["junc"].cpu().numpy() * 7
         jtyp = meta[i]["jtyp"].cpu().numpy()
 
 
-        rjuncs = result["juncs"][i].cpu().numpy() * 6
+        rjuncs = result["juncs"][i].cpu().numpy() * 7
         rjtyp = result["jtype"][i].cpu().numpy()
 
 
-        lpre = meta[i]["lpre"].cpu().numpy() * 6
+        lpre = meta[i]["lpre"].cpu().numpy() * 7
         lpre_label = meta[i]["lpre_label"].cpu().numpy()
         #print("vecl target max", np.max(lpre_label), lpre_label)
-        vecl_result = result["lines"][i].cpu().numpy() * 6
+        vecl_result = result["lines"][i].cpu().numpy() * 7
         print("lines in trainer:",vecl_result.shape, vecl_result[1])
         #print("results for lines",vecl_result.shape, vecl_result[1])
         score = result["score"][i].cpu().numpy()
